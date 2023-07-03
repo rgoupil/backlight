99af6e0dc3d3a1191ee22cacf312729d589b9084
Robin Goupil <goupil.robin@gmail.com>
2023-07-03 15:00:33 +0300

    quick branding

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './back-light-hind-siliguri-regular.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
