57372ccae148833b103f4d58f95e14b4ca2f2518
Robin Goupil <goupil.robin@gmail.com>
2023-07-03 14:39:37 +0300

    add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import RGBColor from 'rgbcolor';
+import './App.scss';
+
+const storageKeys = {
+  backgroundColor: 'background-color',
+};
+
+const defaultColor = '#f7f7f7';
 
 function App() {
+  const [init, setInit] = useState<boolean>(false);
+  const [color, setColor] = useState<string>(defaultColor);
+
+  const { r, g, b } = new RGBColor(color);
+  const y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  const isLight = y > 128;
+
+  useEffect(() => {
+    const data = window.localStorage.getItem(storageKeys.backgroundColor);
+    if (data) {
+      setColor(data);
+    }
+    setInit(true);
+  }, []);
+
+  useEffect(() => {
+    if (!init) {
+      return;
+    }
+    window.localStorage.setItem(storageKeys.backgroundColor, color);
+  }, [init, color]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: color }}>
+      <label htmlFor="input-color" className="App-link">
+        <span className="App-text" style={{ color: isLight ? '#000' : '#fff' }}>{color}</span>
+        <input id="input-color" type="color" value={color} onChange={ev => setColor(ev.target.value)} />
+      </label>
     </div>
   );
 }
---
+++ src/rgbcolor.d.ts
@@ -0,0 +1,9 @@
+declare module 'rgbcolor' {
+  export default class RGBColor {
+    constructor(color: string);
+
+    r: number;
+    g: number;
+    b: number;
+  }
+}
